754908796bba089fc77f0d325919789c15f61009
Brayanklein555 <brayanprogramacao@gmail.com>
2026-04-29 11:44:59 -0300

    Initialize Firebase configuration in firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "COLOQUE_AQUI",
+  authDomain: "COLOQUE_AQUI",
+  databaseURL: "COLOQUE_AQUI",
+  projectId: "COLOQUE_AQUI",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
