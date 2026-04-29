6d8465d1a24267b72fbed13f85192c853e59de92
Brayanklein555 <brayanprogramacao@gmail.com>
2026-04-29 12:36:40 -0300

    Update Firebase configuration details
    Updated Firebase configuration with new credentials.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 const firebaseConfig = {
-  apiKey: "COLOQUE_AQUI",
-  authDomain: "COLOQUE_AQUI",
-  databaseURL: "COLOQUE_AQUI",
-  projectId: "COLOQUE_AQUI",
+  apiKey: "AIzaSyDDDteU9UwXg8cshkQ03jSGe8OZ5xp26dM",
+  authDomain: "ordorpgistas-7b56d.firebaseapp.com",
+  databaseURL: "https://ordorpgistas-7b56d-default-rtdb.firebaseio.com",
+  projectId: "ordorpgistas-7b56d",
+  storageBucket: "ordorpgistas-7b56d.firebasestorage.app",
+  messagingSenderId: "371067328681",
+  appId: "1:371067328681:web:36fab49657197c068a31b8"
 };
 
 firebase.initializeApp(firebaseConfig);
